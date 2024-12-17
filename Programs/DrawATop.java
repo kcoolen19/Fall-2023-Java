9b300611c7c568b398be38c553a027b56b8392e7
Keiron Coolen <151109820+kcoolen19@users.noreply.github.com>
2024-12-17 23:48:58 +0400

    Eliminate Magic Values
    Cleaned the code by storing magic values in variables. Used a test size of 3 for the structure of the output diagram.

--- Programs/DrawATop.java
+++ Programs/DrawATop.java
@@ -1,5 +1,10 @@
 public class DrawATop {
-    public static final int SIZE = 2;
+    public static final int TEST_SIZE = 3;
+    public static final int DOUBLE_SIZE = 2;
+    public static final String STAR_SIGN = "*";
+    public static final String EQUAL_SIGN = "=";
+    public static final String DOUBLE_BAR = "||";
+    public static final String SPACE = " ";
 
     public static void main(String[] args) {
         topPart();
@@ -8,11 +13,11 @@ public class DrawATop {
     }
 
     public static void topPart() {
-        for (int line = 1; line <= SIZE; line++) {
-            for (int space = 1; space <= (SIZE - 1); space++) {
-                System.out.print(" ");
+        for (int line = 1; line <= TEST_SIZE; line++) {
+            for (int space = 1; space <= (TEST_SIZE - 1); space++) {
+                System.out.print(SPACE);
             }
-            System.out.println("||");
+            System.out.println(DOUBLE_BAR);
         }
     }
 
@@ -23,39 +28,39 @@ public class DrawATop {
     }
 
     public static void middleTopPart() {
-        for (int line = 1; line <= SIZE; line++) {
-            for (int space = 1; space <= (SIZE - line); space++) {
+        for (int line = 1; line <= TEST_SIZE; line++) {
+            for (int space = 1; space <= (TEST_SIZE - line); space++) {
                 System.out.print(" ");
             }
             for (int star = 1; star <= (line - 1); star++) {
-                System.out.print("*");
+                System.out.print(STAR_SIGN);
             }
-            System.out.print("||");
+            System.out.print(DOUBLE_BAR);
             for (int star = 1; star <= (line - 1); star++) {
-                System.out.print("*");
+                System.out.print(STAR_SIGN);
             }
             System.out.println();
         }
     }
 
     public static void middleEqualPart() {
-        for (int equal = 1; equal <= (SIZE * 2); equal++) {
-            System.out.print("=");
+        for (int equal = 1; equal <= (TEST_SIZE * DOUBLE_SIZE); equal++) {
+            System.out.print(EQUAL_SIGN);
         }
         System.out.println();
     }
 
     public static void middleBottomPart() {
-        for (int line = 1; line <= SIZE; line++) {
+        for (int line = 1; line <= TEST_SIZE; line++) {
             for (int space = 1; space <= (line - 1); space++) {
-                System.out.print(" ");
+                System.out.print(SPACE);
             }
-            for (int star = 1; star <= (SIZE - line); star++) {
-                System.out.print("*");
+            for (int star = 1; star <= (TEST_SIZE - line); star++) {
+                System.out.print(STAR_SIGN);
             }
-            System.out.print("||");
-            for (int star = 1; star <= (SIZE - line); star++) {
-                System.out.print("*");
+            System.out.print(DOUBLE_BAR);
+            for (int star = 1; star <= (TEST_SIZE - line); star++) {
+                System.out.print(STAR_SIGN);
             }
             System.out.println();
         }
@@ -72,18 +77,18 @@ public class DrawATop {
     }
 
     public static void bottomTwoSticks() {
-        for (int line = 1; line <= SIZE; line++) {
-            for (int space = 1; space <= (SIZE - 1); space++) {
-                System.out.print(" ");
+        for (int line = 1; line <= TEST_SIZE; line++) {
+            for (int space = 1; space <= (TEST_SIZE - 1); space++) {
+                System.out.print(SPACE);
             }
-            System.out.println("||");
+            System.out.println(DOUBLE_BAR);
         }
     }
 
     public static void base() {
-        for (int line = 1; line <= SIZE; line++) {
-            for (int equal = 1; equal <= (SIZE * 2); equal++) {
-                System.out.print("=");
+        for (int line = 1; line <= TEST_SIZE; line++) {
+            for (int equal = 1; equal <= (TEST_SIZE * DOUBLE_SIZE); equal++) {
+                System.out.print(EQUAL_SIGN);
             }
             System.out.println();
         }
