b58c727f1e7047ff3b20f05f4cc90c9a92cdcc21
Keiron Coolen <151109820+kcoolen19@users.noreply.github.com>
2024-12-19 09:44:26 +0400

    Changes
    Eliminate magic values

--- Programs/Staircase.java
+++ Programs/Staircase.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class Staircase {
 
+    public static final String HEIGHT_PROMPT = "Enter the height of the staircase: ";
+    public static final String HASH_SYMBOL = "#";
+    public static final String UNDERSCORE_SYMBOL = "__";
+    public static final int FORMAT_COEFFICIENT = 2;
+
     public static void main(String[] args) { // Main method
-        Scanner Console = new Scanner(System.in);
-        System.out.print("Enter the height of the staircase: ");
+        Scanner console = new Scanner(System.in);
+        System.out.print(HEIGHT_PROMPT);
         // Prompt the user to enter the height for the staircase
-        int height = Console.nextInt(); 
+        int height = console.nextInt(); 
         drawStairCase(height);
     }
 
@@ -14,14 +19,14 @@ public class Staircase {
         // For loop to print the number of lines/height of the staircase
         for (int line = 1; line <= height; line++) { 
             // For loop to print the hash marks
-            for (int hash = 1; hash <= (2 * line - 2); hash++) { 
+            for (int hash = 1; hash <= (FORMAT_COEFFICIENT * line - FORMAT_COEFFICIENT); hash++) { 
                 // Printing the hash marks
-                System.out.print("#"); 
+                System.out.print(HASH_SYMBOL); 
             }
             /* Printing the underscores after the hash marks
             Moving to a new line after printing the hash marks and underscores
              */
-            System.out.println("__");
+            System.out.println(UNDERSCORE_SYMBOL);
         }
     }
 }
